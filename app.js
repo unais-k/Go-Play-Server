5ec73552ba35b4ad2f7d6eaa7dc4495ea154a6ff
unais-k <unaissafa008@gmail.com>
2023-05-25 13:45:00 +0530

    second push

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ import socketConnection from "./Socket/Socket.js";
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:3000/", "https://go-play.onrender.com"],
+    })
+);
 app.use(helmet({ crossOriginResourcePolicy: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.static("public"));
